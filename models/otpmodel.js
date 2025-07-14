70ace36676f02d636375c93a089b5db7164aaaab
Yaswanthram-396 <yaswanthram.396@gmail.com>
2025-07-14 18:07:38 +0530

    corrected otp

---
+++ models/otpmodel.js
@@ -0,0 +1,30 @@
+// models/OTP.js
+const mongoose = require("mongoose");
+
+const otpSchema = new mongoose.Schema(
+  {
+    phoneNumber: {
+      type: String,
+      required: true,
+      index: true,
+    },
+    otp: {
+      type: String,
+      required: true,
+    },
+    expiresAt: {
+      type: Date,
+      required: true,
+      index: { expireAfterSeconds: 0 }, // MongoDB TTL index - auto cleanup
+    },
+    isUsed: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("OTP", otpSchema);
